c9475b6b5e8b8b81c81ece2019e290171206a09b
MuhAbdurrahman <116608696+MuhAbdurrahman@users.noreply.github.com>
2022-10-25 14:23:48 +0800

    Update Landingpage.php

--- Landingpage.php
+++ Landingpage.php
@@ -81,3 +81,32 @@
             </div>
         </div>
     </div>
+
+        <!-- NAVIGATION -->
+    <nav class="fixed top-0 left-0 bg-white w-full shadow-2xl z-50">
+        <div class="container m-auto flex justify-between items-center">
+            <img src="https://media.discordapp.net/attachments/963226363362422794/1023557412524929035/logobrj.png" alt="" id="logo" class="w-32 pl-8 py-4">
+            <ul class="hidden md:flex items-center pr-10 cursor-pointer">
+                <li href="#" class="font-[350] py-4 px-6 hover:border-b-4 rounded-tr-lg rounded-tl-lg border-blue-500 hover:pb-3 transition-all duration-50 ease-in hover:delay-100">Produk</li>
+                <li href="#" class="font-[350] py-4 px-6 hover:border-b-4 rounded-tr-lg rounded-tl-lg border-blue-500 hover:pb-3 transition-all duration-50 ease-in hover:delay-100">Tentang</li>
+                <li href="#" class="font-[350] py-4 px-6 hover:border-b-4 rounded-tr-lg rounded-tl-lg border-blue-500 hover:pb-3 transition-all duration-50 ease-in hover:delay-100">Layanan</li>
+                <li href="#" class="font-[350] py-4 px-6 hover:border-b-4 rounded-tr-lg rounded-tl-lg border-blue-500 hover:pb-3 transition-all duration-50 ease-in hover:delay-100">Blog</li>
+                <li href="#" class="text-blue-500 font-[350] py-4 px-6 hover:border-b-4 rounded-tr-lg rounded-tl-lg border-blue-500 hover:pb-3 transition-all duration-50 ease-in hover:delay-100">Hubungi Kami</li>
+            </ul>
+            <button class="block md:hidden py-3 px-4 mx-2 rounded focus:outline-none hover:bg-gray-200 group">
+                <div class="w-5 h-1 bg-gray-600 mb-1"></div>
+                <div class="w-5 h-1 bg-gray-600 mb-1"></div>
+                <div class="w-5 h-1 bg-gray-600 "></div>
+                <div class="absolute top-0 right-full h-[60vh] w-8/12 bg-white border opacity-0 group-focus:right-0 group-focus:opacity-100 transition-all duration-300">
+                    <ul class="flex flex-col items-center w-full cursor-pointer pt-10">
+                        <li href="#" class="font-[350] py-4 px-6 w-full hover:border-b-4 rounded-tr-lg rounded-tl-lg border-blue-500 hover:pb-3 transition-all duration-50 ease-in hover:delay-100">Produk</li>
+                        <li href="#" class="font-[350] py-4 px-6 w-full hover:border-b-4 rounded-tr-lg rounded-tl-lg border-blue-500 hover:pb-3 transition-all duration-50 ease-in hover:delay-100">Tentang</li>
+                        <li href="#" class="font-[350] py-4 px-6 w-full hover:border-b-4 rounded-tr-lg rounded-tl-lg border-blue-500 hover:pb-3 transition-all duration-50 ease-in hover:delay-100">Layanan</li>
+                        <li href="#" class="font-[350] py-4 px-6 w-full hover:border-b-4 rounded-tr-lg rounded-tl-lg border-blue-500 hover:pb-3 transition-all duration-50 ease-in hover:delay-100">Blog</li>
+                        <li href="#" class="text-blue-500 font-[350] py-4 px-6 w-full hover:border-b-4 rounded-tr-lg rounded-tl-lg border-blue-500 hover:pb-3 transition-all duration-50 ease-in hover:delay-100">Hubungi Kami</li>
+                    </ul>
+                </div>
+            </button>
+        </div>
+    </nav>
+    <!-- END NAV -->
