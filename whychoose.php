a25a6439bb7f30eff0a711e036f767b7d40dcf04
GustiBintangRPL <116608719+GustiBintangRPL@users.noreply.github.com>
2022-10-25 13:47:49 +0800

    Menambahkan Part Bintang "Mengapa Memilih Brj?"

---
+++ whychoose.php
@@ -0,0 +1,62 @@
+<div class="bg-white">
+    
+   <div class="lg:mt-32 mx-auto lg:max-w-5xl " data-aos="fade-up" data-aos-duration="3000">
+    <div class="flex justify-center">
+        <h1 class="text-[#000000] mt-28 text-4xl" data-aos="fade-up" data-aos-duration="20000">
+            Mengapa Memilih BRJ?
+        </h1>
+   </div>
+
+    <div class="flex justify-center">
+        <h5 class="text-[#000000] mt-10 mb-10 " data-aos="fade-up" data-aos-duration="20000">
+        6 Alasan Memilih BRJ: Berkualitas, Cepat, Handal, Fleksibel, Biaya Murah, Ramah
+        </h5>
+        </div>
+    </div>
+
+        <div class="lg:grid lg:grid-cols-2 g:mt-32 lg:gap-x-14 lg:mx-auto lg:max-w-6xl">
+
+        <div class="text-start mb-10 text-white hover:text-black px-9 py-9 rounded-lg bg-[#42C2FF] text-base hover:shadow-lg hover:bg-white transition-all duration-300 hover:shadow-blue-500" data-aos="fade-down" data-aos-duration="2000">
+            <img class="mb-10" src="https://birran.com/Files/Theme/Birran2020/assets/img/icon/icon_berkualitas.png" alt="" srcset="">
+            <h2 class="mb-5 text-2xl">Berkualitas</h2>
+            BRJ memahami bahwa konsistensi dan persepsi produk Anda sangat penting. Itu sebabnya BRJ memastikan bahwa standar kualitas BRJ memenuhi kebutuhan Anda dalam menyelesaikan permasalahan digital.
+        </div>
+
+        <div class="px-5 py-5 mb-10 text-white hover:text-black rounded-lg bg-[#42C2FF] text-base hover:shadow-lg hover:bg-white transition-all duration-300 hover:shadow-blue-500" data-aos="fade-down" data-aos-duration="2000">
+            <img class="mb-10" src="https://birran.com/Files/Theme/Birran2020/assets/img/icon/icon_cepat.png" alt="" srcset="">
+            <h2 class="mb-5 text-2xl">Cepat</h2>
+            BRJ berusaha sebaik mungkin menghadirkan solusi cepat dan mudah agar mengurangi waktu tunggu pelanggan tetapi dengan kualitas yang terjaga.
+        </div>
+
+        <div class="text-white mb-10 hover:text-black px-5 py-5 rounded-lg bg-[#42C2FF] text-base hover:shadow-lg hover:bg-white transition-all duration-300 hover:shadow-blue-500" data-aos="fade-right" data-aos-duration="1500">
+            <img class="mb-10" src="https://birran.com/Files/Theme/Birran2020/assets/img/icon/ic_team.png" alt="" srcset="">
+            <h2 class="mb-5 text-2xl">Handal</h2>
+            Produk kami dirancang dan dibangun oleh para profesional sehingga menghasilkan produk yang handal. Produk kami dilengkapi dengan garansi dan dokumentasi yang lengkap, sehingga memudahkan pelanggan dalam melakukan pemeliharaan.
+        </div>
+
+        <div class="text-white mb-10 hover:text-black text-start px-9 py-9 rounded-lg bg-[#42C2FF] text-base hover:shadow-lg hover:bg-white transition-all duration-300 hover:shadow-blue-500" data-aos="fade-left" data-aos-duration="1500">
+            <img class="mb-10" src="https://birran.com/Files/Theme/Birran2020/assets/img/icon/icon_fleksibel.png" alt="" srcset="">
+            <h2 class="mb-5 text-2xl">Fleksibel</h2>
+            Demi memenuhi kebutuhan pelanggan, BRJ berusaha sefleksibel mungkin dalam menerapkan solusi.
+        </div>
+
+        <div class=" text-white mb-10 hover:text-black text-start px-9 py-9 rounded-lg bg-[#42C2FF] text-base hover:shadow-lg hover:bg-white transition-all duration-300 hover:shadow-blue-500" data-aos="fade-up" data-aos-duration="1500">
+            <img class="mb-10" src="https://birran.com/Files/Theme/Birran2020/assets/img/icon/icon_murah.png" alt="" srcset="">
+            <h2 class="mb-5 text-2xl">Biaya Murah</h2>
+            Demi mengoptimalkan pengeluaran Anda dalam transformasi digital, BRJ berusaha memberikan solusi yang mudah dan murah.
+        </div>
+        
+        <div class=" text-white mb-10 hover:text-black text-start px-9 py-9 rounded-lg bg-[#42C2FF] text-base hover:shadow-lg hover:bg-white transition-all duration-300 hover:shadow-blue-500" data-aos="fade-up" data-aos-duration="1500">
+            <img class="mb-8" src="https://media.discordapp.net/attachments/963226363362422794/1023550592225918986/handshake.png" width="70" alt="" srcset="">
+            <h2 class="mb-5 text-2xl">Ramah</h2>
+            Demi membuat kenyamanan anda, BRJ akan memberikan pelayanan yang ramah dengan 5S.
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    </div>
+    <br>
+    <br>
+    </div>
