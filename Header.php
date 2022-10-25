a84bb4482ed42fd6f3c97aa317a9f51e3b3195b4
Yafi <116608702+yafi124@users.noreply.github.com>
2022-10-25 14:06:54 +0800

    Create Header.php
    Added Section 1 / Header

---
+++ Header.php
@@ -0,0 +1,16 @@
+<div class="mx-auto mt-48" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+        <div class="">
+            <div class="bg-white md:h-96 md:w-3/5 md:mx-12 md:py-5 md:rounded-md md:shadow-2xl mb-10 md:shadow-blue-500 flex items-center justify-center">
+                <div class="mx-32">
+                    <h1 class="text-4xl opacity-70 font-['Nunito Sans'] font-bold select-none">Spesialis</h1>
+                    <br>
+                    <h1 class="text-4xl opacity-70 font-['Nunito Sans'] font-bold select-none">Membantu Pengembangan</h1>
+                    <br>
+                    <h1 class="text-4xl opacity-70 font-['Nunito Sans'] font-bold select-none">Tugas</h1>
+                    <p class="ml-1 mt-6 font-[400] font-['Nunito Sans'] opacity-70 text-lg select-auto">Basecamp menyediakan layanan yang membantu mengerjakan dan <br> pengembangan segala tugas untuk membantu mengoptimalkan nilai dan <br> memperindah warna rapor.</p>
+                    <button class="mt-3 ml-[-6px] py-3 px-3 text-xl font-['Nunito Sans']  uppercase font-[350] opacity-80 hover:opacity-100 hover:border-b-4 rounded-tl-lg rounded-tr-lg border-blue-500 hover:pb-3 transition-all duration-300 ease-in-out hover:delay-50">Hubungi Kami</button>
+                    <button class="py-3 ml-[-6px] px-3 text-xl font-['Nunito Sans'] uppercase font-[350] opacity-80 hover:hover:opacity-100 hover:border-b-4 rounded-tl-lg rounded-tr-lg border-blue-500 hover:pb-3 transition-all duration-300 ease-in-out hover:delay-50">Selengkapnya</button>
+                </div>
+            </div>
+        </div>
+    </div>
