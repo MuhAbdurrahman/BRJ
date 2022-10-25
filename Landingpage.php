c0ac5df74c83f44447a33df668860d617ecc091e
MuhAbdurrahman <116608696+MuhAbdurrahman@users.noreply.github.com>
2022-10-25 14:27:03 +0800

    Update Landingpage.php

--- Landingpage.php
+++ Landingpage.php
@@ -199,3 +199,49 @@
 </div>
 </div>
 </div>
+        
+<div class="w-full block text-center mt-24 bg-white text-black">
+        <img src="https://media.discordapp.net/attachments/963226363362422794/1023557412524929035/logobrj.png" alt="logo" class="mx-auto w-44 pt-8 ">
+       <div class="md:flex mx-auto container p-5 sm:pt-3">
+         <div class="w-4/5 md:w-1/5 mx-auto pt-2">
+          <h2 class="text-sm uppercase tracking-widest p-4">Address</h2>
+          <p class="text-xs font-rubik">
+          Komplek Aulia Permai, Sungai Sipai<br>
+          Telp. : +62 8134 7051 164 , <br>E: BRJ@gmail.com
+          </p>
+              </div>
+              <div class="w-3/5 mx-auto pt-2">
+          <h2 class="text-sm uppercase tracking-widest p-4">Socmed</h2>
+          <div class="inline-flex space-x-3">
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+          width="30" height="30" viewBox="0 0 30 30"
+          style=" fill:#000;">
+          <path d="M15,3C8.373,3,3,8.373,3,15c0,6.016,4.432,10.984,10.206,11.852V18.18h-2.969v-3.154h2.969v-2.099c0-3.475,1.693-5,4.581-5 c1.383,0,2.115,0.103,2.461,0.149v2.753h-1.97c-1.226,0-1.654,1.163-1.654,2.473v1.724h3.593L19.73,18.18h-3.106v8.697 C22.481,26.083,27,21.075,27,15C27,8.373,21.627,3,15,3z"></path></svg>
+          
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+          width="30" height="30"
+          viewBox="0 0 30 30"
+          style=" fill:#000;">
+          <path d="M 9.9980469 3 C 6.1390469 3 3 6.1419531 3 10.001953 L 3 20.001953 C 3 23.860953 6.1419531 27 10.001953 27 L 20.001953 27 C 23.860953 27 27 23.858047 27 19.998047 L 27 9.9980469 C 27 6.1390469 23.858047 3 19.998047 3 L 9.9980469 3 z M 22 7 C 22.552 7 23 7.448 23 8 C 23 8.552 22.552 9 22 9 C 21.448 9 21 8.552 21 8 C 21 7.448 21.448 7 22 7 z M 15 9 C 18.309 9 21 11.691 21 15 C 21 18.309 18.309 21 15 21 C 11.691 21 9 18.309 9 15 C 9 11.691 11.691 9 15 9 z M 15 11 A 4 4 0 0 0 11 15 A 4 4 0 0 0 15 19 A 4 4 0 0 0 19 15 A 4 4 0 0 0 15 11 z"></path></svg>
+
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+          width="30" height="30"
+          viewBox="0 0 30 30"
+          style=" fill:#000;">
+          <path d="M 15 4 C 10.814 4 5.3808594 5.0488281 5.3808594 5.0488281 L 5.3671875 5.0644531 C 3.4606632 5.3693645 2 7.0076245 2 9 L 2 15 L 2 15.001953 L 2 21 L 2 21.001953 A 4 4 0 0 0 5.3769531 24.945312 L 5.3808594 24.951172 C 5.3808594 24.951172 10.814 26.001953 15 26.001953 C 19.186 26.001953 24.619141 24.951172 24.619141 24.951172 L 24.621094 24.949219 A 4 4 0 0 0 28 21.001953 L 28 21 L 28 15.001953 L 28 15 L 28 9 A 4 4 0 0 0 24.623047 5.0546875 L 24.619141 5.0488281 C 24.619141 5.0488281 19.186 4 15 4 z M 12 10.398438 L 20 15 L 12 19.601562 L 12 10.398438 z"></path></svg></div>
+        </div>
+        <div class="w-1/5 mx-auto pt-2">
+          <h2 class="text-sm uppercase tracking-widest p-4 md:text-left">Pages</h2>
+          <ul class="md:pl-4">
+          <li class="text-xs md:text-left capitalize"><a>contact</a></li>
+          <li class="text-xs md:text-left capitalize"><a>terms of use</a></li>
+          <li class="text-xs md:text-left capitalize"><a>privacy policy</a></li>
+          <li class="text-xs md:text-left capitalize"><a>sitemap</a></li>
+          </ul>
+        </div>
+        </div>
+          <span class="text-2xl mb-5">Created By KKN</span> 
+          <br>
+          <br>
+        
+    </div> 
